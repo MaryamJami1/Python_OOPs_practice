426f8707216733b0c585b196e73b8a1d27cdd494
MaryamJami1 <maryamchouhan373@gmail.com>
2025-05-19 02:36:37 +0500

    abstraction

--- Abstraction/Q3.py
+++ Abstraction/Q3.py
@@ -6,3 +6,30 @@ SavingsAccount (withdraw only allowed if balance ≥ amount)
 CurrentAccount (allow overdraft up to a certain limit)"""
 
 
+from abc import ABC , abstractmethod
+
+
+class BankAccount(ABC):
+
+    @abstractmethod
+    def withdraw_amount(self):
+        pass
+
+
+class SavingAccount(BankAccount):
+    def __init__(self, balance, amount):
+        self.balance = balance
+        self.amount = amount
+
+    def withdraw_amount(self):
+        if self.balance > self.amount:
+            print(f"successfully withdrew your amount = {self.amount}")
+
+        else:
+            print("sorry balance is insufficent")
+            
+        
+        
+
+class CurrentAccount(BankAccount):
+    pass
