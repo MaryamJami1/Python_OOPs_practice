4ae46edbf5d2684004c15b11440a730478573522
MaryamJami1 <maryamchouhan373@gmail.com>
2025-05-19 21:54:17 +0500

    duck typing and class variables

--- duck_typing_and_class_variables/Q5.py
+++ duck_typing_and_class_variables/Q5.py
@@ -3,3 +3,27 @@ Create a book object and try to change library using the object
 (e.g., book.library = "Community Library").
 Print the library value using the object and 
 the class name and explain why they are different."""
+
+
+class Book:
+    library = "City Library"
+
+book1 = Book()
+
+book1.library = "Community Library"
+
+print(book1.library)
+print(Book.library)
+
+
+# print(book1.__dict__)
+
+"""
+When we assign book1.library = "Community Library", 
+we are not modifying the class variable. Instead,
+Python creates a new instance variable library for book1. 
+This shadows the class variable. So, accessing book1.
+library returns the instances value,
+while Book.library still returns the original class value.
+To modify a class variable, we should do:
+Book.library = "New Library" instead of modifying through an instance."""
