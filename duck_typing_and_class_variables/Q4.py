1197e4622965fec66cb1b87f03bde0386662675b
MaryamJami1 <maryamchouhan373@gmail.com>
2025-05-19 21:42:59 +0500

    duck typing and class variables

--- duck_typing_and_class_variables/Q4.py
+++ duck_typing_and_class_variables/Q4.py
@@ -3,3 +3,19 @@ Create 2 student objects.
 Change the class variable school to "New School" using the class name.
 Print the school for both students before and after the change."""
 
+
+class Student:
+    school = "old school"
+
+
+
+stdnt1 = Student()
+stdnt2 = Student()
+
+print(stdnt1.school)
+print(stdnt2.school)
+
+Student.school = "New School"
+
+print(stdnt1.school)
+print(stdnt2.school)
