acd70dbdfdd9362652582827cce1e5742f0b7db6
MaryamJami1 <maryamchouhan373@gmail.com>
2025-05-19 21:35:25 +0500

    duck typing and class variables

---
+++ duck_typing_and_class_variables/Q4.py
@@ -0,0 +1,5 @@
+"""Create a class Student with a class variable school = "Old School". 
+Create 2 student objects.
+Change the class variable school to "New School" using the class name.
+Print the school for both students before and after the change."""
+
---
+++ duck_typing_and_class_variables/Q5.py
@@ -0,0 +1,5 @@
+"""Create a class Book with a class variable library = "City Library".
+Create a book object and try to change library using the object
+(e.g., book.library = "Community Library").
+Print the library value using the object and 
+the class name and explain why they are different."""
